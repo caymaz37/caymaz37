2f9b36d6101ad9020a65eb13b9c22a9f04ac897e
Yaşar Caymaz <32465832+caymaz37@users.noreply.github.com>
2024-12-06 03:51:23 +0300

    Update settings.py

--- toner/settings.py
+++ toner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6h5!qkzh(n#5^kh^2wdz1a544vmp9h=mite2@8x&@+wd2h^d(_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
